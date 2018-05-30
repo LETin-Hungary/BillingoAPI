db46e130064795eaf5715c8cf52db7de0d1d536b
Csutorás Robin <csutorasr@gmail.com>
2018-05-30 15:39:05 +0200

    fix: properties mapped to json

--- src/Models/BillingoError.cs
+++ src/Models/BillingoError.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace LETin.BillingoAPI.Models
 {
     public class BillingoError
     {
+        [JsonProperty("error")]
         public string Error { get; set; }
+        [JsonProperty("errors")]
         public ICollection<string> Errors { get; set; }
+        [JsonProperty("success")]
         public bool Success { get; set; }
     }
 }
--- src/Models/BillingoResponse.cs
+++ src/Models/BillingoResponse.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace LETin.BillingoAPI.Models
 {
     public class BillingoResponse<T>
     {
+        [JsonProperty("type")]
         public string Type { get; set; }
+        [JsonProperty("data")]
         public ICollection<T> Data { get; set; }
+        [JsonProperty("success")]
         public bool Success { get; set; }
     }
 }
